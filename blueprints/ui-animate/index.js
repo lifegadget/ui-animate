b5ad84bc84de47e1ee8ff0c066efa6fbc27c0581
Ken Snyder <ken@ken.net>
2016-04-05 15:50:32 +0100

    added blueprint to install bower deps

---
+++ blueprints/ui-animate/index.js
@@ -0,0 +1,17 @@
+/*jshint node:true*/
+module.exports = {
+  description: ''
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  afterInstall: function(options) {
+    return this.addBowerPackagesToProject([
+      { name: 'animate.css' }
+    ]);
+  }
+};
