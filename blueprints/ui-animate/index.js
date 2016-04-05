78c984082b9b2567ff4a5670d2f0a84e0e724cec
Ken Snyder <ken@ken.net>
2016-04-05 15:56:42 +0100

    bower dep works

--- blueprints/ui-animate/index.js
+++ blueprints/ui-animate/index.js
@@ -2,6 +2,7 @@
 module.exports = {
   description: 'install some great CSS animations to use out of the box',
 
+  normalizeEntityName: function() {},
   afterInstall: function(options) {
     return this.addBowerPackageToProject('animate.css');
   }
